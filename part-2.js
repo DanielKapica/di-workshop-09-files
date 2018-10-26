d75b5836f7037c90e75ff0dff485930322632088
Daniel Kapica <kapica6105@gmail.com>
2018-10-26 12:43:10 +0100

    Completed Task 2

--- part-2.js
+++ part-2.js
@@ -1,1 +1,9 @@
-console.log('your code here')
+var fs = require('fs')
+var fileContents = fs.readFileSync('shopping-basket.json', 'utf-8')
+var data = JSON.parse(fileContents)
+for (var item of data.basket) {
+    //console.log(item.name)
+    //console.log(item.price)
+    //console.log(item.quantity)
+    console.log(item.name + " $" + item.price * item.quantity)
+  }
